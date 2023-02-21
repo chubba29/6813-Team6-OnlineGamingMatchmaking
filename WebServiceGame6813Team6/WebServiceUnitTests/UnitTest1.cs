390c67bff62a9d7e4fe7a0c874df2c361ad2b200
Cliff Cross <cliff.cross@stjude.org>
2023-02-21 10:45:07 -0600

    AB#11: Added unit testing framework

---
+++ WebServiceGame6813Team6/WebServiceUnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace WebServiceUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testValue = 1;
+            Assert.AreEqual(1, testValue);
+        }
+    }
+}
