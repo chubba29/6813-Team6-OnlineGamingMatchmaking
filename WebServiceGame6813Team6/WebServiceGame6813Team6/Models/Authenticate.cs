6834bc22af3dda7fcb735fb2cde73b3a6ae0dc6a
crossc <cliff.cross@alsac.stjude.org>
2023-04-11 23:56:27 -0500

    AB#29: Implemented Basic Authentication and added it to Users Controller

---
+++ WebServiceGame6813Team6/WebServiceGame6813Team6/Models/Authenticate.cs
@@ -0,0 +1,14 @@
+﻿namespace WebServiceGame6813Team6.Models;
+
+using System.ComponentModel.DataAnnotations;
+
+public class Authenticate
+{
+
+    [Required]
+    public string Username { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
+
