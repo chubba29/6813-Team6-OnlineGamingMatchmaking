b6d750d445d4c3d732683fc18d6e09cc028c16e5
Christopher Hubbard <chubba29@students.kennesaw.edu>
2023-04-17 19:49:06 -0500

    basic login page

---
+++ WebApplicationGame6813Team6/WebApplicationGame6813Team6/Pages/Profile.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplicationGame6813Team6.Pages
+{
+    public class ProfileModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public ProfileModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
