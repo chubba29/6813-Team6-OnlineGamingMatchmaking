14754916a64cf7310be37c3f85637e9262fba70f
Christopher Hubbard <christopher.hubbard@alsac.stjude.org>
2023-04-18 20:41:20 -0500

    session and state management https://learn.microsoft.com/en-us/aspnet/core/fundamentals/app-state?view=aspnetcore-7.0

--- WebApplicationGame6813Team6/WebApplicationGame6813Team6/Pages/Login.cshtml.cs
+++ WebApplicationGame6813Team6/WebApplicationGame6813Team6/Pages/Login.cshtml.cs
@@ -4,7 +4,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace WebApplicationGame6813Team6.Pages
 {
     public class LoginModel : PageModel
+
     {
+        public const string SessionKeyName = "_Name";
+        public const string SessionKeyAge = "_Age";
+        public const string AuthHeader = "_AuthHeader";
         private readonly ILogger<LoginModel> _logger;
 
         public LoginModel(ILogger<LoginModel> logger)
@@ -14,7 +18,19 @@ namespace WebApplicationGame6813Team6.Pages
 
         public void OnGet()
         {
-
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                HttpContext.Session.SetString(SessionKeyName, "The Doctor");
+                //username: testChris
+                //password: testChris
+                HttpContext.Session.SetString(AuthHeader, "Basic dGVzdENocmlzOnRlc3RDaHJpcw==");
+                HttpContext.Session.SetInt32(SessionKeyAge, 73);
+            }
+            var name = HttpContext.Session.GetString(SessionKeyName);
+            var age = HttpContext.Session.GetInt32(SessionKeyAge).ToString();
+            
+            _logger.LogInformation("Session Name: {Name}", name);
+            _logger.LogInformation("Session Age: {Age}", age);
         }
     }
 }
