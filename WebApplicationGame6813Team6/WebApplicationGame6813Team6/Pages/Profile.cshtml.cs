9b05dce3320c526cba0bc79faba965c7cb014749
Christopher Hubbard <christopher.hubbard@alsac.stjude.org>
2023-04-23 12:30:42 -0500

    update to profiles page and unique value on USERID in profiles table

--- WebApplicationGame6813Team6/WebApplicationGame6813Team6/Pages/Profile.cshtml.cs
+++ WebApplicationGame6813Team6/WebApplicationGame6813Team6/Pages/Profile.cshtml.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplicationGame6813Team6.Pages
 {
-    public class GamePreferences : PageModel
+    public class Profile : PageModel
     {
-        private readonly ILogger<GamePreferences> _logger;
+        private readonly ILogger<Profile> _logger;
 
-        public GamePreferences(ILogger<GamePreferences> logger)
+        public Profile(ILogger<Profile> logger)
         {
             _logger = logger;
         }
